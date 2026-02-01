46485c348fd2937789e7b36fb9281f88ba95b62a
rizkyfahmi <rzkyfhm@gmail.com>
2026-02-01 17:44:25 +0700

    fix: import

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
 import sendEmail from "../configs/nodeMailer.js";
+import Connection from "../models/Connection.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "my-app" });
